24e8643db0465f67edc5556a6e5910ffc8a1eb4b
sioodmy <hello@sioodmy.dev>
2024-05-21 00:24:16 +0200

    feat: add website

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"embed"
 	"flag"
+	"io/fs"
 	"net/http"
 
 	"github.com/sioodmy/lyricsapi/internal/get"
 )
 
+//go:embed static
+var static embed.FS
+
+func serve_static() http.Handler {
+	sub, err := fs.Sub(static, "static")
+	if err != nil {
+		panic(err)
+	}
+	file_server := http.FileServer(http.FS(sub))
+	return file_server
+}
+
 func main() {
 	listen := flag.String("l", "0.0.0.0:3000", "IP and port to listen on")
 	mux := http.NewServeMux()
 
+	mux.Handle("/", serve_static())
 	mux.HandleFunc("/api/{query}", get.GetHandle)
 
 	flag.Parse()
